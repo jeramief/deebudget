fb44df9b86ac213530400ff4b9ab24d4de478b5d
jeramief <109633173+jeramief@users.noreply.github.com>
2023-06-03 22:08:52 -0400

    seeded database

--- server/server.js
+++ server/server.js
@@ -1,17 +1,24 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const helmet = require("helmet");
-const morgan = require("morgan");
-// import express from "express"
-// import bodyParser from "body-parser";
-// import mongoose from "mongoose";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import helmet from "helmet";
-// import morgan from "morgan";
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+// const dotenv = require("dotenv");
+// const helmet = require("helmet");
+// const morgan = require("morgan");
+// const kpiRoutes = require("./routes/kpi");
+// const KPI = require("./models/KPI.js");
+// const { kpis } = require("./data/data.js")
+import express from "express"
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import kpiRoutes from "./routes/kpi.js";
+import KPI from "./models/KPI.js";
+import { kpis } from "./data/data.js";
+
 
 // CONFIGURATIONS
 dotenv.config();
@@ -22,6 +29,28 @@ app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
 app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// ROUTES
+app.use("/kpi", kpiRoutes)
+
 
 // MONGOOSE SETUP
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.DB_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+  })
+  .then(async () => {
+    console.log("DB CONNECTED")
+    app.listen(PORT, () => {
+      console.log(`Port: ${PORT}`)
+    })
+
+    // ADD DATA ONE TIME ONLY OR AS NEEDED
+    // await mongoose.connection.db.dropDatabase();
+    // KPI.insertMany(kpis);
+  })
+  .catch((err) => console.log(err.message));
